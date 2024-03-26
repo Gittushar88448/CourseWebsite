5172689801675cea5badd740e8bde98e9101264e
Tushar <kmrtushar01@gmail.com>
2024-03-26 12:51:53 +0530

    tailwind css addidng to nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,9 +3,11 @@ import React from 'react'
 const NavBar = () => {
   return (
     <div>
-        <h1>
+      <nav className="bg-bgDark py-4">
+        <h1 className="text-3xl font-bold text-center text-white">
             TOP COURSES
         </h1>
+      </nav>
     </div>
   )
 }
